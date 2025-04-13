fa2faddd7f7e722b2288f675c11fe8dc497ce238
olekszczepanowski <olekszczepanowski04@wp.pl>
2025-04-13 19:32:53 +0200

    feat: completed user mutations

--- list_7/src/resolvers/Mutation.js
+++ list_7/src/resolvers/Mutation.js
@@ -1,88 +1,167 @@
-const { ApolloError, UserInputError } = require('apollo-server-errors');
-const { Todo } = require('../models');
-const { syncTodosFromREST, syncUsersFromREST } = require('../sync/syncFunctions');
+const { ApolloError, UserInputError } = require("apollo-server-errors");
+const { Todo, User } = require("../models");
+const {
+  syncTodosFromREST,
+  syncUsersFromREST,
+} = require("../sync/syncFunctions");
 
 const Mutation = {
-    syncTodos: async () => {
-        return await syncTodosFromREST();
-    },
-    syncUsers: async () => {
-        return await syncUsersFromREST();
-    },
-    createTodo: async (parent, { input }) => {
-        try {
-            if (!input.userId || input.userId.toString().trim() === '') {
-                throw new UserInputError("todo must be assigned to a valid user");
-            }
-            const userId = Number(input.userId);
-            
-            const existingUser = await User.findOne({ _id: userId });
-            if (!existingUser) {
-                throw new UserInputError(`user with id ${input.userId} does not exist.`);
-            }
-            
-            const maxTodo = await Todo.findOne().sort({ _id: -1 }).exec();
-            const newId = maxTodo ? maxTodo._id + 1 : 1;
-        
-            const newTodoData = {
-                _id: newId,
-                title: input.title,
-                completed: input.completed,
-                userId: Number(input.userId) 
-            };
-        
-            const newTodo = await Todo.create(newTodoData);
-            return newTodo;
-        } catch (error) {
-            throw new ApolloError("error creating todo: " + error.message);
-        }
-    },
-    updateTodo: async (parent, { id, input }) => {
-        try {
-            if (!id || id.trim() === '') {
-                throw new UserInputError("incorrect todo id");
-            }
+  syncTodos: async () => {
+    return await syncTodosFromREST();
+  },
 
-            const updatedTodo = await Todo.findOneAndUpdate(
-                { _id: Number(id) },
-                input,
-                { new: true, runValidators: true }
-            );
+  syncUsers: async () => {
+    return await syncUsersFromREST();
+  },
 
-            if (!updatedTodo) {
-                throw new UserInputError(`todo with id ${id} not found`);
-            }
+  createTodo: async (parent, { input }) => {
+    try {
+      if (!input.userId || input.userId.toString().trim() === "") {
+        throw new UserInputError("todo must be assigned to a valid user");
+      }
+      const userId = Number(input.userId);
 
-            return updatedTodo;
-        } catch (error) {
-            if (error && error.name === 'UserInputError') {
-                throw error;
-            }
+      const existingUser = await User.findOne({ _id: userId });
+      if (!existingUser) {
+        throw new UserInputError(
+          `user with id ${input.userId} does not exist.`
+        );
+      }
 
-            throw new ApolloError("error updating todo: " + error.message);
-        }
-    },
-    deleteTodo: async (parent, { id }) => {
-        try {
-            if (!id || id.trim() === '') {
-                throw new UserInputError("incorrect todo id");
-            }
+      const maxTodo = await Todo.findOne().sort({ _id: -1 }).exec();
+      const newId = maxTodo ? maxTodo._id + 1 : 1;
 
-            const deletedTodo = await Todo.findOneAndDelete({ _id: Number(id) });
+      const newTodoData = {
+        _id: newId,
+        title: input.title,
+        completed: input.completed,
+        userId: userId,
+      };
 
-            if (!deletedTodo) {
-                throw new UserInputError(`todo with id ${id} not found`);
-            }
+      const newTodo = await Todo.create(newTodoData);
+      return newTodo;
+    } catch (error) {
+      throw new ApolloError("error creating todo: " + error.message);
+    }
+  },
 
-            return deletedTodo;
-        } catch (error) {
-            if (error && error.name === 'UserInputError') {
-                throw error;
-            }
+  updateTodo: async (parent, { id, input }) => {
+    try {
+      if (!id || id.trim() === "") {
+        throw new UserInputError("incorrect todo id");
+      }
 
-            throw new ApolloError("error deleting todo: " + error.message);
-        }
-    }   
+      const updatedTodo = await Todo.findOneAndUpdate(
+        { _id: Number(id) },
+        input,
+        { new: true, runValidators: true }
+      );
+
+      if (!updatedTodo) {
+        throw new UserInputError(`todo with id ${id} not found`);
+      }
+
+      return updatedTodo;
+    } catch (error) {
+      if (error && error.name === "UserInputError") {
+        throw error;
+      }
+
+      throw new ApolloError("error updating todo: " + error.message);
+    }
+  },
+
+  deleteTodo: async (parent, { id }) => {
+    try {
+      if (!id || id.trim() === "") {
+        throw new UserInputError("incorrect todo id");
+      }
+
+      const deletedTodo = await Todo.findOneAndDelete({ _id: Number(id) });
+
+      if (!deletedTodo) {
+        throw new UserInputError(`todo with id ${id} not found`);
+      }
+
+      return deletedTodo;
+    } catch (error) {
+      if (error && error.name === "UserInputError") {
+        throw error;
+      }
+
+      throw new ApolloError("error deleting todo: " + error.message);
+    }
+  },
+
+  createUser: async (parent, { input }) => {
+    try {
+      const maxUser = await User.findOne().sort({ _id: -1 }).exec();
+      const newId = maxUser ? maxUser._id + 1 : 1;
+
+      const newUser = await User.create({
+        _id: newId,
+        name: input.name,
+        email: input.email,
+        login: input.login,
+      });
+
+      return newUser;
+    } catch (error) {
+      throw new ApolloError("error creating user: " + error.message);
+    }
+  },
+
+  updateUser: async (parent, { id, input }) => {
+    try {
+      if (!id || id.trim() === "") {
+        throw new UserInputError("incorrect user id");
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: Number(id) },
+        input,
+        { new: true, runValidators: true }
+      );
+
+      if (!updatedUser) {
+        throw new UserInputError(`user with id ${id} not found`);
+      }
+
+      return updatedUser;
+    } catch (error) {
+      if (error && error.name === "UserInputError") {
+        throw error;
+      }
+
+      throw new ApolloError("error updating user: " + error.message);
+    }
+  },
+
+  deleteUser: async (parent, { id }) => {
+    try {
+      if (!id || id.trim() === "") {
+        throw new UserInputError("incorrect user id");
+      }
+
+      const userId = Number(id);
+
+      const deletedUser = await User.findOneAndDelete({ _id: userId });
+
+      if (!deletedUser) {
+        throw new UserInputError(`user with id ${id} not found`);
+      }
+
+      await Todo.deleteMany({ userId: userId });
+
+      return deletedUser;
+    } catch (error) {
+      if (error && error.name === "UserInputError") {
+        throw error;
+      }
+
+      throw new ApolloError("error deleting user: " + error.message);
+    }
+  },
 };
 
 module.exports = Mutation;
